529c3325dc549bccf548be9c90cc35e70d0bad1d
kenichi noshian <nosikenit@gmail.com>
2022-06-29 20:44:58 +0900

    feature works section

---
+++ pages/components/Works/Works.module.scss.d.ts
@@ -0,0 +1,6 @@
+export const swiper: string;
+export const swiperDiscription: string;
+export const swiperTech: string;
+export const swiperTitle: string;
+export const title: string;
+export const worksWrapper: string;
