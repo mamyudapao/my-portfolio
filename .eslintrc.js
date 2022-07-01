da8823962b5fc63fb4de966487873e122811abed
kenichi noshian <nosikenit@gmail.com>
2022-07-01 23:22:56 +0900

    テスト2

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     rules: {
         "require-jsdoc": "off",
         "react/react-in-jsx-scope": "off",
+        "no-console": "error",
     },
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  console.log("aaaa");
   return <></>;
 };
 
