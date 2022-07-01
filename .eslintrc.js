650546918b46c38b9546ed7cd5a89f4c84e09975
kenichi noshian <nosikenit@gmail.com>
2022-07-01 23:21:38 +0900

    テスト

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ["plugin:react/recommended", "google", "prettier"],
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "google",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+    ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaFeatures: {
